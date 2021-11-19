6e6af0ebd1afb4a50eee19b321bd41e56f6405c0
vic-sh <victor.shalaginov@gmail.com>
2021-11-19 12:09:20 -0500

    adding test_unique_id_4.py

---
+++ ch2/tasks_proj/tests/func/test_unique_id_4.py
@@ -0,0 +1,25 @@
+import pytest
+import tasks
+
+
+@pytest.mark.xfail(tasks.__version__ < '0.2.0',
+                   reason='not supported until version 0.2.0')
+def test_unique_id_1():
+    """Calling unique_id() twice should return different numbers"""
+    id_1 = tasks.unique_id()
+    id_2 = tasks.unique_id()
+    assert id_1 != id_2
+
+
+@pytest.mark.xfail()
+def test_unique_id_is_a_duck():
+    """Demonstarte xfail."""
+    uid = tasks.unique_id()
+    assert uid == 'a duck'
+
+
+@pytest.mark.xfail()
+def test_unique_id_not_a_duck():
+    """Demonstrate xpass."""
+    uid = tasks.unique_id()
+    assert uid != 'a duck'
