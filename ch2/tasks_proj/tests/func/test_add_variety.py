c70c83f169c8f6cf2e4c2e4026031e55cfeba6dc
vic-sh <victor.shalaginov@gmail.com>
2021-11-24 20:19:26 -0500

    adding test_add_5 to test_add_variety

--- ch2/tasks_proj/tests/func/test_add_variety.py
+++ ch2/tasks_proj/tests/func/test_add_variety.py
@@ -39,11 +39,10 @@ def test_add_2(task):
 
 
 @pytest.mark.parametrize('summary, owner, done',
-[('sleep', None, False),
-('wake', 'brian', False),
-('breathe', 'BRIAN', True),
-('eat eggs', 'BrIaN', False),
-])
+                       [('sleep', None, False),
+                        ('wake', 'brian', False),
+                        ('breathe', 'BRIAN', True),
+                        ('eat eggs', 'BrIaN', False)])
 def test_add_3(summary, owner, done):
     """Demonstrate parametrize with multiple parameters"""
     task = Task(summary, owner, done)
@@ -51,15 +50,29 @@ def test_add_3(summary, owner, done):
     t_from_db = tasks.get(task_id)
     assert equivalent(t_from_db, task)
 
+
 tasks_to_try = (Task('sleep', done=True),
                 Task('wake', 'brian'),
                 Task('wake', 'brian'),
                 Task('breathe', 'BRIAN', True),
                 Task('exercise', 'BrIaN', False))
 
+
 @pytest.mark.parametrize('task', tasks_to_try)
 def test_add_4(task):
     """Slightly different take."""
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
+
+
+task_ids = ['Task({},{},{})'.format(t.summary, t.owner, t.done)
+            for t in tasks_to_try]
+
+
+@pytest.mark.parametrize('task', tasks_to_try, ids=task_ids)
+def test_add_5(task):
+    """Demonstrate ids."""
     task_id = tasks.add(task)
     t_from_db = tasks.get(task_id)
     assert equivalent(t_from_db, task)
