86d873ab83d4261d5d141f57391c91710dfbf726
vic-sh <victor.shalaginov@gmail.com>
2021-11-25 12:13:29 -0500

    adding TestAdd class

--- ch2/tasks_proj/tests/func/test_add_variety.py
+++ ch2/tasks_proj/tests/func/test_add_variety.py
@@ -76,3 +76,20 @@ def test_add_5(task):
     task_id = tasks.add(task)
     t_from_db = tasks.get(task_id)
     assert equivalent(t_from_db, task)
+
+
+@pytest.mark.parametrize('task', tasks_to_try, ids=task_ids)
+class TestAdd():
+    """Demonstrate parametrize and test classes."""
+
+    def test_equivalent(self, task):
+        """Similar test, just within a class."""
+        task_id = tasks.add(task)
+        t_from_db = tasks.get(task_id)
+        assert equivalent(t_from_db, task)
+
+    def test_valid_id(self, task):
+        """We can use the same data or multiple tests."""
+        task_id = tasks.add(task)
+        t_from_db = tasks.get(task_id)
+        assert t_from_db.id == task_id
