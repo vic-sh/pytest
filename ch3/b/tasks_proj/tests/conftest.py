5b6d8fc1e8b6c61c8c7fd4615820909c703392cf
vic-sh <victor.shalaginov@gmail.com>
2022-01-21 08:26:46 -0500

    üpdating conftest in b folder

--- ch3/b/tasks_proj/tests/conftest.py
+++ ch3/b/tasks_proj/tests/conftest.py
@@ -32,3 +32,21 @@ def tasks_just_a_few():
         Task('Write some code', 'Brian', True),
         Task("Code review Brian's code", 'Katie', False),
         Task('Fix what Brian did', 'Michelle', False))
+
+
+@pytest.fixture(scope='session')
+def tasks_mult_per_owner():
+    """Several owners with several tasks each."""
+    return(
+        Task('Make a cookie', 'Raphael'),
+        Task('Use an emoji', 'Raphael'),
+        Task('Move to berlin', 'Raphael'),
+
+        Task('Create', 'Michelle'),
+        Task('Inspire', 'Michelle'),
+        Task('Encourage', 'Michelle'),
+
+        Task('Do a handstand', 'Daniel'),
+        Task('Write some books', 'Daniel'),
+        Task('Eat ice cream', 'Daniel'),
+    )
